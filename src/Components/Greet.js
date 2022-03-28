ef72b8ba6999223f47b38318ad35e8f4ca2e6e9f
AyeshaAreej <ayeshaareej222@gmail.com>
2022-03-28 21:36:51 +0500

    function & class based components

---
+++ src/Components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Greet(props) {
+  return (
+    <div>Function Component<br/>
+    <h1>Hello this is from function component</h1>
+    </div>
+  )
+}
---
+++ src/Components/Welcome.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Welcome extends Component{
+
+    render(){
+
+        return <>
+            Class Component
+            <h1>This is from class component</h1>
+
+        </>
+    }
+}
+export default Welcome;
