c1d89f97e3289aa2267657d0ed9011bed5510677
AyeshaAreej <ayeshaareej222@gmail.com>
2022-04-01 00:13:17 +0500

    initial app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+
 
 function App() {
   return (
     <div className="App">
-     <ClickCounter/>
-     <HoverCounter/>
+   
     </div>
   );
 }
