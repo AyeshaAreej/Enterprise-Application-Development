ee1c18bba32bb5226e80ef7302a4c619463eaeb9
AyeshaAreej <ayeshaareej222@gmail.com>
2022-04-02 19:44:08 +0500

    Context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/UserContext';
+import React, { Component } from 'react'
 
+export class App extends Component {
+  render() {
+    return (
+      <div>
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+        <UserProvider value="Ayesha">
+          <ComponentC/>
+        </UserProvider>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
