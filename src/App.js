4f2f52d55feee1ff2fa3fe486984ecf3c42002be
AyeshaAreej <ayeshaareej222@gmail.com>
2022-04-05 11:58:14 +0500

    grayimage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-
+import cat from './cat.png'
 import React, { Component } from 'react'
 
 export class App extends Component {
+
   render() {
+    console.log(cat);
     return (
       <div>
+        
+       <h1>hello</h1>
+       <img src={cat} alt="Logo" className='gray' />
 
-        <h1>hello</h1>
-       
+
+
       </div>
     )
   }
-}
+}                                                                                   
 
 export default App
