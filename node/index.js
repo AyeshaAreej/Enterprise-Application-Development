9ed6ed2561220c7cc07390694c0ab9335e06af1c
AyeshaAreej <ayeshaareej222@gmail.com>
2022-05-23 01:01:21 +0500

    Products added

--- node/index.js
+++ node/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const Product= require('./Product') 
 
+//mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test',
+{useNewUrlParser: true,
+useUnifiedTopology: true}).then(()=>{
+    console.log('connection is successfully')
+}).catch((err)=>{
+    console.log(err);
+});
+ 
+ 
 // Setting EJS as template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,7 +24,6 @@ app.use(express.static('public/css/'))
 
 
 app.get('/', (req, res) => {
-    // const products=['A','B','C','D','E','F']
     res.render('home', { name: 'Ayesha Areej'})
 })
 
@@ -25,10 +36,23 @@ app.use('/products', (req,res)=>{
     const products=['A','B','C','D','E','F']
     res.render('products', { name: 'Ayesha Areej', products })
 })
+
+app.use('/create', (req,res)=>{
+    res.render('create')
+})
 //error message 
 app.use('*', (req, res)=>{
     res.status(404).json({"msg":"Not Found"})
 })
+
+
+// creating and posting product
+app.post("/product/create", function(req, res) {
+    Product.create(req.body, function(err, product){
+     console.log(product)
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
