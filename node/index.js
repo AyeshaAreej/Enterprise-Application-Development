470985fe85cccfb62acbb828d7df56372399d20c
AyeshaAreej <ayeshaareej222@gmail.com>
2022-05-22 22:29:53 +0500

    Dynamic rendering using express and ejs

--- node/index.js
+++ node/index.js
@@ -2,12 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Setting EJS as templating engine
+// Setting EJS as template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+//adding public folder
+app.use(express.static('public/css/'))
+
+
 app.get('/', (req, res) => {
-    res.render('home', { name: 'Monroe' })
+    // const products=['A','B','C','D','E','F']
+    res.render('home', { name: 'Ayesha Areej'})
+})
+
+app.use('/about', (req,res)=>{
+    res.render('about', { name: 'Ayesha Areej' })
+})
+
+app.use('/products', (req,res)=>{
+  
+    const products=['A','B','C','D','E','F']
+    res.render('products', { name: 'Ayesha Areej', products })
+})
+//error message 
+app.use('*', (req, res)=>{
+    res.status(404).json({"msg":"Not Found"})
 })
 
 app.listen(3000, () => {
